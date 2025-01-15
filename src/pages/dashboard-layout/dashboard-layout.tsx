77b1033d7bb301268d5a3cfb12b03c7375f64534
Sopiko Imnaishvili <sopho.imnaishvili@gmail.com>
2025-01-15 16:54:48 +0400

    remove navbar

--- src/pages/dashboard-layout/dashboard-layout.tsx
+++ src/pages/dashboard-layout/dashboard-layout.tsx
@@ -6,12 +6,9 @@ import { useTranslation } from "react-i18next";
 import { USERS_PATHS } from "./users/users-routes";
 import { QUESTIONS_PATHS } from "./questions/question-routes";
 
-const { Header, Content, Footer, Sider } = Layout;
+const {Content, Footer, Sider } = Layout;
 
-const items1: MenuProps["items"] = ["1", "2", "3"].map((key) => ({
-  key,
-  label: `nav ${key}`,
-}));
+
 
 const DashboardLayout: React.FC = () => {
   const { t } = useTranslation();
@@ -40,16 +37,7 @@ const DashboardLayout: React.FC = () => {
 
   return (
     <Layout>
-      <Header style={{ display: "flex", alignItems: "center" }}>
-        <div className="demo-logo" />
-        <Menu
-          theme="dark"
-          mode="horizontal"
-          defaultSelectedKeys={["2"]}
-          items={items1}
-          style={{ flex: 1, minWidth: 0 }}
-        />
-      </Header>
+    
       <Content className="m-10">
         <Layout
           style={{
