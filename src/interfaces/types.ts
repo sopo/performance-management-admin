3af88e51bc4799c5f9a94a7a64f9193083e9ae04
Sopiko Imnaishvili <sopho.imnaishvili@gmail.com>
2025-01-15 13:39:01 +0400

    add add profile request

--- src/interfaces/types.ts
+++ src/interfaces/types.ts
@@ -1,5 +1,7 @@
-import { Database } from "../supabase/database.types";
+import { Database, TablesInsert } from "../supabase/database.types";
 
 export type QuestionsListProps =
   Database["public"]["Tables"]["questions"]["Insert"][];
 export type QuestionProps = Database["public"]["Tables"]["questions"]["Insert"];
+
+export type ProfileInsert = TablesInsert<"profiles">
